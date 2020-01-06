2941707d028dc04b94f0ea9972eaaa6ff6ef347c
Hernan <iphone1198@icloud.com>
2020-01-06 10:03:01 -0500

    add

---
+++ src/main/java/com/springwebflux/bootcamp/service/creditcards/app/SpringwebfluxBootcampServiceCreditCardsApplication.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.creditcards.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxBootcampServiceCreditCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxBootcampServiceCreditCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springwebflux/bootcamp/service/creditcards/app/SpringwebfluxBootcampServiceCreditCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.creditcards.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxBootcampServiceCreditCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
